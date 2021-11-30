d355ccfd84a1c3df4520779e1d821c83b13b2798
Bas Baccarne <bastiaan.baccarne@ugent.be>
2021-11-30 16:46:54 +0100

    final

--- arduino_sketch.ino
+++ arduino_sketch.ino
@@ -1,22 +1,31 @@
 // Open this file (or copy/paste) in the Arduino IDE to upload the script to your Arduino
-// Make sure you selected the Arduino Nano as your board (& have the associated packages installed)
+// Make sure you selected the Arduino Nano (or other Arduino) as your board (& have the associated packages installed)
 // Make sure the right COM port is selected
 
-// pinout settings
-int sensorPin = A0;
-int maximum = 0;
+// libraries
+#include <Wire.h>
+#include "Adafruit_MPRLS.h"
+
+// settings
+#define RESET_PIN  -1  
+#define EOC_PIN    -1 
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("MPRLS Simple Test");
+  if (! mpr.begin()) {
+    Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("Found MPRLS sensor");
 }
 
+
 void loop() {
-  // read the input data
-  int reading = analogRead(sensorPin);  
-
-  // serial print data
-   Serial.println(reading);
-
-  // set delay
-  delay(200);
+  float pressure_hPa = mpr.readPressure();
+  Serial.println(pressure_hPa / 68.947572932);
+  delay(500);
 }
